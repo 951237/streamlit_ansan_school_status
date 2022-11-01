4483496bd4d51ff0f2bb3e9da921aad73ad80dff
951237 <951237@gmail.com>
2022-11-02 00:53:47 +0900

    Update : 데이터 저장 폴더 및 크롤 앱 만들기

---
+++ crawl_data.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import lxml
+import plotly.express as px
+import streamlit as st
+import matplotlib.pyplot as plt
+
+def read_html():
+	URL  = 'https://www.goeas.kr/USR/ORG/MNU13/SchoolList.do?orgType=Z'
+	df = pd.read_html(URL, header=1)[0]
+	df = df[:5]   # '계' 열 삭제
+ 
+	# 공립, 사립 칼럼의 '-' 없애기
+	def remove_str(col):
+		return col.strip().replace("-","0")
+
+	df['국립'] = df['국립'].apply(remove_str)
+	df['공립'] = df['공립'].apply(remove_str)
+	df['사립'] = df['사립'].apply(remove_str)
+ 
+	# 공립, 사립 칼럼 숫자로 바꾸기
+	# df_spc['가점_기타'] = df_spc['가점_기타'].astype(str)
+	df['국립'] = df['국립'].astype(int)
+	df['공립'] = df['공립'].astype(int)
+	df['사립'] = df['사립'].astype(int)
+	return df 
+
+df = read_html()
+df.to_csv('/data/data.csv')
